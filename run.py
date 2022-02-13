2c1f0c5d40a48336dc5773b00ab9769b152e67da
Samantha Kwenaeetile <p2525129@my365.dmu.ac.uk>
2022-02-13 23:29:32 +0000

    Add varibales and random choice of words for the game from wordlist

--- run.py
+++ run.py
@@ -1,3 +1,5 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random 
+
+wordlist = [ foxglove ,frazzled ,frizzled , blitz , blizzard , boggle , stretch , stronghold ,stymied ,zigzagging , zilch , zipper , zodiac ,zombie , equip , cycle ,jinx ,larynx , vixen]
+
+word = random.choice(wordlist).lower()
