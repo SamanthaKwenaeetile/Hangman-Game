935be44253b44b8715dfe497452104bc030af665
Samantha Kwenaeetile <p2525129@my365.dmu.ac.uk>
2022-02-13 23:38:16 +0000

    Add have error on line 19

--- run.py
+++ run.py
@@ -3,3 +3,18 @@ import random
 wordlist = [ foxglove ,frazzled ,frizzled , blitz , blizzard , boggle , stretch , stronghold ,stymied ,zigzagging , zilch , zipper , zodiac ,zombie , equip , cycle ,jinx ,larynx , vixen]
 
 word = random.choice(wordlist).lower()
+
+guessed_correctly = []
+
+guessed_incorrectly = []
+
+tires = 5
+
+hangman_count = -1 
+while tries > 0:
+    output =''
+    for letter in word:
+        if letter in guessed_correctly:
+            output += letter
+            else:
+                output +='_'
