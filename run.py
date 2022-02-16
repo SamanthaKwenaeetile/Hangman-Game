565281de308962571fa8a48e171ca5156ec21cdf
Samantha Kwenaeetile <p2525129@my365.dmu.ac.uk>
2022-02-16 00:39:44 +0000

    Add error on line of code input name

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
-import random 
+import random
+def thehangman():
 
-name = input("enter your name here")
-print("Welcome",name, "!")
+name = input("enter your name here ")
+print("Welcome" ,name, "To The Hangman Game")
 print("------------------")
-print("try to guess the word you will have 5 goes!!")
+print("try to guess the words you will have 5 goes!!")
+
+thehangman()
