a4a863fc312a33f0473e04f5fb351aca4021de9b
Samantha Kwenaeetile <p2525129@my365.dmu.ac.uk>
2022-02-16 01:05:22 +0000

    Add code for guess the word and if its valid or not what to do

--- run.py
+++ run.py
@@ -4,9 +4,29 @@ def thehangman():
 words_list = [ 'apple' , 'apricots' , 'avocado' ,'banana' , 'blackberries' ,'blackcurrant' ,'blueberries']
 
 word = random.choice(words_list)
+
+turns = 5
+guessmade = ''
+valid_entry = set('abcdefghijklmnopqrstuvwxyz')
+
+while len(word)>0:
+    main_word = ""
+    missed = 0
+
+    print("guess the word", main_word)
+    guess = input()
+
+    if guess in valid_entry:
+        guessmade = guessmade+guess
+
+    else:
+            print("enter the right character")
+            guess = input()
+
+
 Name = input("enter your name here ")
-print("Welcome",name, "To The Hangman Game")
+print("Welcome",name, "To The Hangman Game fruit Guessing Game ")
 print("------------------")
-print("try to guess the words you will have 5 goes!!")
+print("You Have 5 Guess Good Luck!!")
 
 thehangman()
