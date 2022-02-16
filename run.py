0f7cbb437247cffc9559284baf0d33060046abd4
Samantha Kwenaeetile <p2525129@my365.dmu.ac.uk>
2022-02-16 00:33:03 +0000

    Add code for user to enter name they will use when playing game and attempts they will have

--- run.py
+++ run.py
@@ -1,20 +1,6 @@
 import random 
 
-wordlist = [ foxglove ,frazzled ,frizzled , blitz , blizzard , boggle , stretch , stronghold ,stymied ,zigzagging , zilch , zipper , zodiac ,zombie , equip , cycle ,jinx ,larynx , vixen]
-
-word = random.choice(wordlist).lower()
-
-guessed_correctly = []
-
-guessed_incorrectly = []
-
-tires = 5
-
-hangman_count = -1 
-while tries > 0:
-    output =''
-    for letter in word:
-        if letter in guessed_correctly:
-            output += letter
-            else:
-                output +='_'
+name = input("enter your name here")
+print("Welcome",name, "!")
+print("------------------")
+print("try to guess the word you will have 5 goes!!")
