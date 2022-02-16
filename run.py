9bb5408f73ac55958498e67c523d7081e3849113
Samantha Kwenaeetile <p2525129@my365.dmu.ac.uk>
2022-02-16 01:38:46 +0000

    error found in my code line 38 need to fix

--- run.py
+++ run.py
@@ -1,11 +1,11 @@
 import random
 def thehangman():
 
-words_list = [ 'apple' , 'apricots' , 'avocado' ,'banana' , 'blackberries' ,'blackcurrant' ,'blueberries']
+word_list = [ 'apple' , 'apricots' , 'avocado' ,'banana' , 'blackberries' ,'blackcurrant' ,'blueberries']
 
-word = random.choice(words_list)
+word = random.choice(word_list)
 
-turns = 5
+turns = 10
 guessmade = ''
 valid_entry = set('abcdefghijklmnopqrstuvwxyz')
 
@@ -20,6 +20,11 @@ while len(word)>0:
     else:
         main_word=main_word+"_"
 
+        if main_word == word:
+            print(main_word)
+            print("well done you have won! ;)")
+            break
+
     print("guess the word", main_word)
     guess = input()
 
@@ -30,6 +35,75 @@ while len(word)>0:
             print("enter the right character")
             guess = input()
 
+if guess not in the word:
+    turns = turns -0
+
+if turns == 10:
+    Print("You have ten turns left")
+    Print("----------")
+
+if turns == 9:
+    print("You have nine turns left")
+    Print("----------")
+    Print("     O     ")
+
+if turns == 8:
+    Print("You have eight turns left")
+    Print("----------")
+    Print("     O     ")
+    Print("     |    ")
+    
+if turns == 7:
+    Print("You seven turns left")
+    Print("----------")
+    Print("     O     ")
+    Print("     |    ")
+
+if turns == 6:
+    Print("You have six turns left")
+    Print("----------")
+    Print("     O     ")
+    Print("     |    ")
+    Print("     /    ")
+   
+if turns == 5:
+    Print("You have five turns left")
+    Print("----------")
+    Print("     O     ")
+    Print("     |    ")
+    Print("    / \  ")
+
+if turns == 4:
+    Print("You have four turns left")
+    Print("----------")
+    Print("    \ O     ")
+    Print("     |    ")
+    Print("    / \  ")
+    
+if turns == 3:
+    Print("You have three turns left")
+    Print("----------")
+    Print("    \ O /   ")
+    Print("      |    ")
+    Print("     / \  ")
+
+if turns == 2:
+    Print("You have two turns left")
+    Print("----------")
+    Print("    \ O /      | ")
+    Print("      |    ")
+    Print("     / \  ")
+
+if turns == 1:
+    Print("You have one turn left")
+    Print("----------")
+    Print("    \ O /   ")
+    Print("      |    ")
+    Print("     / \  ")
+
+if turns == 0:
+    Print("You have lost!!!")
+    Print(" try you luck next time")
 
 Name = input("enter your name here ")
 print("Welcome",name, "To The Hangman Game fruit Guessing Game ")
